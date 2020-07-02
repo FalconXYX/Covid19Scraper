0d8ea2d7ed3c3d6a982be77d58001b699aba7e9d
Parth <parthshreya@hotmail.com>
2020-07-01 21:18:05 -0400

    Update CoronaScraper.py

--- CoronaScraper.py
+++ CoronaScraper.py
@@ -7,20 +7,27 @@ res.raise_for_status()
 ConfimedCasesHTML = []
 ConfimedCasesSepperated = []
 ConfimedCases = []
+Deaths = []
+ProbableCases = []
 
 noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
-for x in range(1,15):
-    number = ""
-    a = str(x)
-    bob = noStarchSoup.select('#dataTable > tbody > tr:nth-child('+a+') > td:nth-child(2)')
-    ConfimedCasesHTML.append(bob)
-    bob = str(bob)
-    lol = re.findall("\d", bob)
-    ConfimedCasesSepperated.append(lol)
-    k = len(lol)
-    for j in range(0,k):
-        number += lol[j]
-    ConfimedCases.append(number)
+def scrape(colum, regex, variable):
+    for x in range(1,15):
+        number = ""
+        a = str(x)
+        bob = noStarchSoup.select('#dataTable > tbody > tr:nth-child('+a+') > td:nth-child('+colum+')')
+        ConfimedCasesHTML.append(bob)
+        bob = str(bob)
+        lol = re.findall(regex, bob)
+        ConfimedCasesSepperated.append(lol)
+        k = len(lol)
+        for j in range(0,k):
+            number += lol[j]
+        variable.append(number)
+scrape("2", "\d", ConfimedCases)
+scrape("3", "\d", Deaths)
+scrape("4", "\d", ProbableCases)
 
-print(ConfimedCasesSepperated)
 print(ConfimedCases)
+print(Deaths)
+print(ProbableCases)
