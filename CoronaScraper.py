c662dca7b33afead51d6f6c9a336bea05281ea6c
Parth <parthshreya@hotmail.com>
2020-07-03 21:41:06 -0400

    Update CoronaScraper.py
    Added threading

--- CoronaScraper.py
+++ CoronaScraper.py
@@ -1,15 +1,19 @@
 import bs4
 import requests
 import re
+import threading, time
+import matplotlib.pyplot as plt
 res = requests.get('https://www.canada.ca/en/public-health/services/diseases/2019-novel-coronavirus-infection.html#a1')
 type(res)
+
 res.raise_for_status()
 ConfimedCasesHTML = []
 ConfimedCasesSepperated = []
 ConfimedCases = []
 Deaths = []
 ProbableCases = []
-
+Area = []
+c = ["r","m","y","b", "g"]
 noStarchSoup = bs4.BeautifulSoup(res.text, 'html.parser')
 def scrape(colum, regex, variable):
     for x in range(1,15):
@@ -18,16 +22,23 @@ def scrape(colum, regex, variable):
         bob = noStarchSoup.select('#dataTable > tbody > tr:nth-child('+a+') > td:nth-child('+colum+')')
         ConfimedCasesHTML.append(bob)
         bob = str(bob)
-        lol = re.findall(regex, bob)
+        print(bob)
+        lol = re.sub(regex,"", bob)
         ConfimedCasesSepperated.append(lol)
-        k = len(lol)
-        for j in range(0,k):
-            number += lol[j]
-        variable.append(number)
-scrape("2", "\d", ConfimedCases)
-scrape("3", "\d", Deaths)
+        line = int(colum)
+        if(line != 1):
+            print(line)
+            lol = int(lol)
+        else:
+            lol=lol[1:len(lol)-1]
+        variable.append(lol)
 
-
-print(ConfimedCases)
-print(Deaths)
-print(ProbableCases)
+threadObj = threading.Thread(target=scrape("1", "<.*?>", Area))
+threadObj.start()
+threadObj2 = threading.Thread(target=scrape("2", "\D", ConfimedCases))
+threadObj2.start()
+threadObj3 = threading.Thread(target=scrape("3", "\D", Deaths))
+threadObj3.start()
+plt.legend(Area)
+plt.pie(ConfimedCases[1:15], labels=Area[1:15],colors = c)
+plt.show()
